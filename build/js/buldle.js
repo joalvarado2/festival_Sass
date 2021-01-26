f413cf16573935aab371d06fa94a97ed0a81a49e
joalvarado2 <jonathanalva92@gmail.com>
2021-01-26 17:09:45 -0500

    agragando el scroll co js y posisionando la barra con fixed

--- build/js/buldle.js
+++ build/js/buldle.js
@@ -1,4 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    scrollNav();
 
+    navegacionFija();
+});
+
+function navegacionFija() {
+    const barra = document.querySelector(".header");
+
+    //registrar el intersection Observer
+    const observer = new IntersectionObserver(entries =>
+        entries[0].isIntersecting
+       ? barra.classList.remove('fijo')
+       : barra.classList.add('fijo')
+       )
+
+    // elemento a observar
+    observer.observe(document.querySelector(".sobre-festival"));
+};
+
+function scrollNav() {  // funcion para hacer scroll en los enlaces dentro de la pagina
+    const enlaces = document.querySelectorAll(".navegacion-principal a");
+    enlaces.forEach( function(enlace) {
+        enlace.addEventListener("click", function(e) {
+            e.preventDefault();
+            const seccion = document.querySelector(e.target.attributes.href.value);
+
+            seccion.scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+}
 document.addEventListener("DOMContentLoaded", function() {
     crearGaleria();
 })
